854c08bed4d344ddbdf5cb146d65335910dad054
Danny Montenegro <djmonten@espol.edu.ec>
2020-06-25 12:39:32 -0500

    Creacion de la Clase Postre y cambios en la clase Helado y Pastel

---
+++ TallerSOLID/src/Postres/Postre.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Postres;
+
+import Adicionales.Aderezo;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author danny
+ */
+public class Postre {
+    protected String sabor;
+    protected double precioParcial;
+    protected ArrayList<Aderezo> aderezos;
+    
+    public Postre(String sabor){
+        aderezos= new ArrayList<>();
+        this.sabor=sabor;
+    }
+    
+    public double calcularPrecioFinal(){
+        double precioFinal;
+        precioFinal=(precioParcial+(precioParcial*0.12))+(aderezos.size()*0.50);
+        return precioFinal;
+    }
+    
+    public ArrayList<Aderezo> getAderezos() {
+        return aderezos;
+    }
+
+    @Override
+    public String toString() {
+        return "Helado{" + "sabor=" + sabor + ", precioParcial=" + precioParcial + ", aderezos=" + aderezos + '}';
+    }
+    
+    public String showPrecioFinal(){
+        return "Precio Final: $ " + calcularPrecioFinal();
+    }
+}
